e7c38f9bf1d6895c47f579eb53caa53782244655
Anant Kumar <anantkumar11@gmail.com>
2018-06-14 10:52:07 -0400

    Initial Commit

---
+++ app/src/main/java/pro/anant/android/balloontext/MainActivity.java
@@ -0,0 +1,13 @@
+package pro.anant.android.balloontext;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
